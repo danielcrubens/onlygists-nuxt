87ba7d7dcde984ade7b66c13957f5a7a99820cc1
danielcrubens <dcr.tecnologi@gmail.com>
2024-03-06 17:37:44 -0300

    configurando layout :fire:

--- app.vue
+++ app.vue
@@ -1,6 +1,5 @@
 <template>
-  <div>
-    <h1 class="font-[Inter] text-3xl font-bold underline text-red-500">Teste1</h1>
-    <Button  label="Submit" />
-  </div>
+<NuxtLayout>
+  <NuxtPage />
+  </NuxtLayout>
 </template>
