e3300f3cf292742104538113e1aae811d9584c67
danielcrubens <dcr.tecnologi@gmail.com>
2024-03-07 11:42:09 -0300

    módulo de reports, criação WidgetGroup e WidgetCondensed :fire:

---
+++ modules/reports/components/Widget/Condensed/Condensed.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="flex flex-col bg-white p-5 rounded-xl">
+    <span class="font-[Inter] text-4xl text-primary-500 font-bold">{{ props.value }}</span>
+    <p class="font-[Inter] text-xl text-gray-700">{{ props.label }}</p>
+  </div>
+</template>
+
+<script setup lang="ts">
+const props = defineProps<{
+  value: number | string
+  label: string
+}>()
+</script>
+
--- modules/users/screens/PublicProfile/PublicProfile.vue
+++ modules/users/screens/PublicProfile/PublicProfile.vue
@@ -1,6 +1,14 @@
 <template>
  <PublicHeadline/>
+ <WidgetGroup>
+  <WidgetCondensed :value="10" label="Gist  do total"/>
+  <WidgetCondensed :value="5" label="Gist gratuitos"/>
+  <WidgetCondensed :value="5" label="Gist pagos"/>
+ </WidgetGroup>
 </template>
 <script setup lang="ts">
 import PublicHeadline from '@/modules/users/components/PublicHeadline/PublicHeadline.vue'
+import WidgetGroup from '@/modules/reports/components/Widget/Group/Group.vue'
+import WidgetCondensed from '@/modules/reports/components/Widget/Condensed/Condensed.vue'
+
 </script>
