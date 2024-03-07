075a34eac05a6d8a6c8fcc5d42441ab5283281f2
danielcrubens <dcr.tecnologi@gmail.com>
2024-03-06 22:35:40 -0300

    módulo de users, criação PublicHeadline :fire:

---
+++ modules/users/components/PublicHeadline/PublicHeadline.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="w-full  flex flex-col items-center justify-center gap-2">
+    <div class="w-24 md:w-32 rounded-full overflow-hidden">
+      <img class="w-full" :src="props.avatarUrl" alt="foto do usuário">
+    </div>
+    <h1 class="font-[Inter] text-gray-700 text-center font-bold text-2xl md:text-3xl">{{ props.name }}</h1>
+    <p class="font-[Inter] text-gray-700 text-center font-regular md:text-lg" v-if="props.bio">{{ props.bio }}</p>
+    <p class="font-[Inter] text-gray-500 text-sm font-regular md:text-base" v-if="props.city && props.state">
+      <i class="pi pi-map-marker"></i>
+      {{ props.city }}, {{props.state}}</p>
+
+  </div>
+</template>
+
+<script setup lang="ts">
+const props = withDefaults(
+  defineProps<{
+  name: string
+  avatarUrl:string
+  bio?: string
+  city?: string
+  state?: string
+}>(),
+{
+  name: 'Daniel Rubens',
+  avatarUrl:'https://avatars.githubusercontent.com/u/52468589?v=4',
+  bio: 'Software Engineer',
+  city: 'Sorocaba',
+  state: 'SP',
+},
+)
+</script>
---
+++ modules/users/screens/PublicProfile/PublicProfile.vue
@@ -0,0 +1,6 @@
+<template>
+ <PublicHeadline/>
+</template>
+<script setup lang="ts">
+import PublicHeadline from '@/modules/users/components/PublicHeadline/PublicHeadline.vue'
+</script>
