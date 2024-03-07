823e047783e6051cb2954d4e5a26c76e97b74ae2
danielcrubens <dcr.tecnologi@gmail.com>
2024-03-07 14:28:59 -0300

    loaders  WidgetGroup  :fire:

---
+++ modules/reports/components/Widget/Group/Loader.vue
@@ -0,0 +1,22 @@
+<template>
+ <template v-if="props.loading">
+  <div v-for="loader in props.amount" :key="`loader-${loader}`" class="flex flex-col justify-center gap-2 bg-white p-5 rounded w-full">
+    <Skeleton width="5rem" height="3rem" />
+    <Skeleton width="100%" height="2rem" />
+  </div>
+ </template>
+ <slot v-else/>
+</template>
+
+<script setup lang="ts">
+const props = withDefaults(
+  defineProps<{
+    loading: boolean
+    amount: number
+  }>(),
+  {
+    loading: false,
+    amount: 4,
+  },
+)
+</script>
--- modules/users/screens/PublicProfile/PublicProfile.vue
+++ modules/users/screens/PublicProfile/PublicProfile.vue
@@ -1,14 +1,17 @@
 <template>
- <PublicHeadline/>
- <WidgetGroup>
-  <WidgetCondensed :value="10" label="Gist  do total"/>
-  <WidgetCondensed :value="5" label="Gist gratuitos"/>
-  <WidgetCondensed :value="5" label="Gist pagos"/>
- </WidgetGroup>
+  <PublicHeadline />
+  <WidgetGroup>
+    <WidgetGroupLoader :loading="false" :amount="3">
+      <WidgetCondensed :value="10" label="Gist  do total" />
+      <WidgetCondensed :value="5" label="Gist gratuitos" />
+      <WidgetCondensed :value="5" label="Gist pagos" />
+    </WidgetGroupLoader>
+  </WidgetGroup>
 </template>
+
 <script setup lang="ts">
 import PublicHeadline from '@/modules/users/components/PublicHeadline/PublicHeadline.vue'
 import WidgetGroup from '@/modules/reports/components/Widget/Group/Group.vue'
+import WidgetGroupLoader from '@/modules/reports/components/Widget/Group/Loader.vue'
 import WidgetCondensed from '@/modules/reports/components/Widget/Condensed/Condensed.vue'
-
 </script>
