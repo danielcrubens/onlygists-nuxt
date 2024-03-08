f30a7f7458e28dddff086e0161f572ace6f5852f
danielcrubens <dcr.tecnologi@gmail.com>
2024-03-08 18:08:07 -0300

    criando GistCodeSnippet  e configurando syntax highlighting :fire:

--- modules/gists/screens/MoreDetail/MoreDetail.vue
+++ modules/gists/screens/MoreDetail/MoreDetail.vue
@@ -1,7 +1,9 @@
 <template>
   <PublicHeadline/>
+  <GistCodeSnippet/>
 </template>
 
 <script setup lang="ts">
-import PublicHeadline from '@/modules/gists/PublicHeadline/PublicHeadline.vue'
+import PublicHeadline from '@/modules/gists/components/PublicHeadline/PublicHeadline.vue'
+import GistCodeSnippet from '@/modules/gists/components/CodeSnippet/CodeSnippet.vue'
 </script>
