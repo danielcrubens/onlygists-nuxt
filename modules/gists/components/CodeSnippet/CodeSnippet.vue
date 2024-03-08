310a2d29f85069c6be319498429555d735c9a9e0
danielcrubens <dcr.tecnologi@gmail.com>
2024-03-08 18:38:34 -0300

    criando loader  GistCodeSnippet   :fire:

--- modules/gists/components/CodeSnippet/CodeSnippet.vue
+++ modules/gists/components/CodeSnippet/CodeSnippet.vue
@@ -1,20 +1,21 @@
 <template>
+  <Loader :loading="props.loading || loading">
   <div class="w-full relative" v-if="props.isPaid">
     <span class="absolute top-[43%] left-[50%] z-[999]">
       <i class="pi pi-lock text-3xl text-gray-700"></i>
     </span>
-
     <pre :class="{ 'blur-sm': props.isPaid }" class="w-full select-none rounded bg-gray-200 p-5 overflow-x-hidden"
       v-html="htmlCode"></pre>
   </div>
   <pre v-if="!props.isPaid" class="w-full rounded bg-gray-200 p-5 overflow-x-scroll" v-html="htmlCode"></pre>
-
+</Loader>
 </template>
 
 <script setup lang="ts">
 import { common, createStarryNight } from '@wooorm/starry-night'
 import { toHtml } from 'hast-util-to-html'
 import '@wooorm/starry-night/style/light'
+import Loader from './Loader.vue'
 const DEFAULT_CODE_SNIPPET = `
 const message = 'Você precisa pagar para ter acesso a esse gist 😛'
 console.log(message)
