cccc9c4aed178f8e8cad18640fbf5d008e56d281
danielcrubens <dcr.tecnologi@gmail.com>
2024-03-08 12:19:17 -0300

    criando tela de detalhe do gist e PublicHeadline  :fire:

---
+++ modules/gists/PublicHeadline/PublicHeadline.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="w-full flex flex-col justify-center gap-2 my-5">
+    <div class="flex flex-wrap items-center gap-2">
+          <h1 class="font-[Inter] text-gray-700 font-bold text-2xl md:text-3xl">{{ props.title }}</h1>
+          <Chip class="text-sm" :label="props.lang" icon="pi pi-bolt" />
+        </div>
+       <div v-html="description" class="font-[Inter] text-gray-700 font-regular text-lg"></div>
+       <p class="font-[Inter] font-regular text-gray-500 text-sm md:text-base">
+       <i class="pi pi-star"></i>by
+       <NuxtLink :to="`/user/${props.author}`">@{{ props.author }}</NuxtLink>
+      </p>
+  </div> 
+</template>
+
+<script setup lang="ts">
+const {render} = useMarkdown()
+const props = withDefaults(
+  defineProps<{
+title: string
+lang: string
+description: string
+author: string
+  }>(),
+  {
+    title:'currtency_format.py',
+    lang:'python',
+    description:'função em python para fazer **formatação de moeda**',
+    author:'daniel'
+  },
+)
+const description = computed(() => {
+  return render(props.description)
+})
+</script>
---
+++ modules/gists/screens/MoreDetail/MoreDetail.vue
@@ -0,0 +1,7 @@
+<template>
+  <PublicHeadline/>
+</template>
+
+<script setup lang="ts">
+import PublicHeadline from '@/modules/gists/PublicHeadline/PublicHeadline.vue'
+</script>
