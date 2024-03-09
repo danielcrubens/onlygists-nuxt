296dc1940741755878a6d8d53cc662ff011ff0ff
danielcrubens <dcr.tecnologi@gmail.com>
2024-03-09 16:50:49 -0300

    DialogPaymentSucess com defineModel em ação   :fire:

--- modules/payments/components/DialogPaymentSuccess/DialogPaymentSuccess.vue
+++ modules/payments/components/DialogPaymentSuccess/DialogPaymentSuccess.vue
@@ -1,5 +1,20 @@
 <template>
-  foi
-  </template>
-  <script setup lang="ts">
+  <Dialog v-model:visible="visible" modal header="Compra efetuada com sucesso!" :style="{ width: '400px' }">
+    <div class="m-0 flex flex-col items-center justify-center w-full">
+      <i class="pi pi-check-circle text-primary-500 text-6xl my-5"></i>
+      <p class="font-[Inter] text-gray-700 text-center">
+        Parabéns pela compra! <br />
+        Você receberá no e-mail fornecido no checkout o seu gist 🤩
+      </p>
+    </div>
+    <template #footer>
+      <div class="flex justify-center w-full">
+        <Button label="Voltar" icon="pi pi-arrow-left" @click="visible = false" autofocus />
+      </div>
+    </template>
+  </Dialog>
+</template>
+
+<script setup lang="ts">
+  const visible = defineModel<boolean>('visible')
 </script>
