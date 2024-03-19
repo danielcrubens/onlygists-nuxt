7e5e46317e35dd8b3a779b509fb1e640eb59bab8
danielcrubens <dcr.tecnologi@gmail.com>
2024-03-19 09:46:53 -0300

    estrutura layout admin, middleware de auth  :fire:

---
+++ modules/reports/screens/Panel/Panel.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+Panel
+  </div>
+</template>
+
+<script>
+export default {
+  setup () {
+    
+
+    return {}
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
---
+++ pages/app/panel.vue
@@ -0,0 +1,11 @@
+<template>
+  <Panel/>
+</template>
+
+<script setup lang="ts">
+import Panel from '@/modules/reports/screens/Panel/Panel.vue'
+definePageMeta({
+  layout: 'admin',
+  middleware: ['auth'],
+})
+</script>
