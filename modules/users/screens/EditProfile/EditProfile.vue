7f04f7f99e208c520fae2652fea6ef9bba90be6a
danielcrubens <dcr.tecnologi@gmail.com>
2024-03-24 11:27:01 -0300

    criando HeadlineEdit, tela de edição perfil   :fire:

---
+++ modules/users/screens/EditProfile/EditProfile.vue
@@ -0,0 +1,7 @@
+<script setup lang="ts">
+import HeadlineEdit from '@/modules/users/components/HeadlineEdit/HeadlineEdit.vue'
+</script>
+
+<template>
+    <HeadlineEdit/>
+</template>
---
+++ pages/app/profile/edit.vue
@@ -0,0 +1,16 @@
+<template>
+  <EditProfile />
+</template>
+
+<script setup lang="ts">
+import EditProfile from '@/modules/users/screens/EditProfile/EditProfile.vue'
+
+useHead({
+  title: 'Editar perfil | 🤩 onlygists',
+})
+
+definePageMeta({
+  layout: 'admin',
+  middleware: ['auth'],
+})
+</script>
