812aa1b33773a05fba8688af61e832f589375251
danielcrubens <dcr.tecnologi@gmail.com>
2024-03-27 20:48:15 -0300

    useAddressUpdate, chamada da busca por cep

--- modules/users/screens/EditProfile/EditProfile.vue
+++ modules/users/screens/EditProfile/EditProfile.vue
@@ -22,18 +22,30 @@ import HeadlineEdit from '@/modules/users/components/HeadlineEdit/HeadlineEdit.v
 import HeadlineEditLoader from '@/modules/users/components/HeadlineEdit/Loader.vue'
 import BasicInfoForm from '@/modules/users/components/BasicInfoForm/BasicInfoForm.vue'
 import AddressForm from '@/modules/users/components/AddressForm/AddressForm.vue'
+import { useAddressUpdate } from '@/modules/users/composables/useAddressUpdate/useAddressUpdate'
 import { myselfKey } from '@/modules/users/composables/useMyself/useMyself'
 import { useUserProfileActions } from '@/modules/users/composables/useUserProfileActions/useUserProfileActions'
 import type { MyselfContextProvider } from '@/modules/users/composables/useMyself/types'
 
 const {user, loading} = inject(myselfKey) as MyselfContextProvider
 const router = useRouter()
+const { share } = useUserProfileActions()
+
+const {
+  loading: addressLoading,
+  searchZipCode,
+  address,
+} = useAddressUpdate({
+  user,
+})
+const handleZipCodeSearch = () => {
+  searchZipCode()
+}
 const handleShare = (username:string) => {
   share(username)
 }
 const handleNavigateToProfile = (username:string) => {
   router.push(`/${username}`)
 }
-const { share } = useUserProfileActions()
 
 </script>
