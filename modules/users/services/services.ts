a20ea9a4c92b58d869c85216401273d67bff9b33
danielcrubens <dcr.tecnologi@gmail.com>
2024-03-27 21:47:08 -0300

    criando serviço de update de user :fire:

--- modules/users/services/services.ts
+++ modules/users/services/services.ts
@@ -1,5 +1,6 @@
 import type { SupabaseClient } from '@supabase/supabase-js'
 import type { Database } from '@/libs/supabase/schema'
+import type { User } from '@/modules/users/entities/User/User'
 import { getMyselfAdapter, searchAddressByZipCodeAdapter } from './adapters'
 import type { AxiosInstance } from 'axios'
 import type { SearchAddressResponse } from './types'
@@ -19,5 +20,28 @@ export default (client: SupabaseClient<Database>, httpClient: AxiosInstance) => 
     const response = await client.from('profiles').select('*').eq('id', id).limit(1).single()
     const user = getMyselfAdapter(response.data)
     return user
+  },
+
+  async update(id: string, { name, site, bio, phone, address }: User) {
+    await client
+      .from('profiles')
+      .update({
+        name,
+        site,
+        bio,
+        phone,
+        address: {
+          zipCode: address?.zipCode,
+          number: address?.number,
+          street: address?.street,
+          city: address?.city,
+          state: address?.state,
+          neighborhood: address?.neighborhood,
+          complement: address?.complement,
+        },
+      })
+      .eq('id', id)
+
+    return { id }
   },
 })
