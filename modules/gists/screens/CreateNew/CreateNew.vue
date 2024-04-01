896044bb11dd6f3a93b7e2ae1d71d59b0b704eb6
danielcrubens <dcr.tecnologi@gmail.com>
2024-04-01 14:17:12 -0300

    criação de rota para criar gist :white_check_mark:

---
+++ modules/gists/screens/CreateNew/CreateNew.vue
@@ -0,0 +1,9 @@
+<template>
+create new
+</template>
+
+<script setup lang="ts">
+import { myselfKey } from '@/modules/users/composables/useMyself/useMyself'
+import type { MyselfContextProvider } from '@/modules/users/composables/useMyself/types'
+const {user, loading}= inject(myselfKey) as MyselfContextProvider
+</script>
